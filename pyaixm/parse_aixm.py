348b323fbc17423500aa60557db3c1351180b1c7
Ben Boyle <142893892+ben-volant@users.noreply.github.com>
2025-04-08 16:32:16 +0100

    Parse multiple files / replace nested XLinks
    * Test
    
    * Fix a couple of yaml issues
    
    * Support simultaneous parsing of multiple files
    
    * Handle nested xlink replacement
    
    * Accidentally a print
    
    * Update aixm_schema.yaml
    
    * Update aixm_schema.yaml
    
    * Update aixm_schema.yaml
    
    * Missed case

--- pyaixm/parse_aixm.py
+++ pyaixm/parse_aixm.py
@@ -11,20 +11,48 @@ def replace_xlinks(features: list):
     "Replaces XLink references on all features with the referenced object"
     for feature in features:
         if isinstance(feature, aixm_types.Feature):
-            for field in fields(feature):
-                attr = getattr(feature, field.name)
-                if isinstance(attr, aixm_types.XLink):
-                    if attr.target is not None:
-                        setattr(feature, field.name, attr.target)
+            replace_xlinks_r(feature, path=[feature.gmlid])
 
 
-def parse(f: typing.IO, resolve_xlinks = False) -> list:
-    context = etree.iterparse(f)
+def replace_xlinks_r(feature: aixm_types.Feature, path: list = []):
+    new_path = path
+    for field in fields(feature):
+        if field.name == "parent":
+            continue
+        attr = getattr(feature, field.name)
+        if isinstance(attr, list):
+            for i, a in enumerate(attr):
+                if isinstance(a, aixm_types.Feature):
+                   replace_xlinks_r(a, path=new_path + [a.gmlid])
+                if isinstance(a, aixm_types.XLink):
+                    if a.target is not None:
+                        attr[i] = a.target
+        if isinstance(attr, aixm_types.Feature):
+            if attr.gmlid in path:
+                continue
+            new_path.append(attr.gmlid)
+            replace_xlinks_r(attr, path=new_path)
+        if isinstance(attr, aixm_types.XLink):
+            if attr.target is not None:
+                setattr(feature, field.name, attr.target)
+
+
+def parse(f: typing.IO | list[typing.IO], resolve_xlinks = False) -> list:
     l = []
-    for action, elem in context:
-        if action == 'end' and elem.tag in ['{http://www.aixm.aero/schema/5.1.1/message}hasMember', '{http://www.aixm.aero/schema/5.1/message}hasMember']:
-            feature = aixm_types.parse_feature(elem[0])  # first child of 'hasMember' is aixm feature
-            l.append(feature)
+    if isinstance(f, list):
+        for g in f:
+            context = etree.iterparse(g)
+            for action, elem in context:
+                if action == 'end' and elem.tag in ['{http://www.aixm.aero/schema/5.1.1/message}hasMember', '{http://www.aixm.aero/schema/5.1/message}hasMember']:
+                    feature = aixm_types.parse_feature(elem[0])  # first child of 'hasMember' is aixm feature
+                    l.append(feature)
+
+    else:
+        context = etree.iterparse(f)
+        for action, elem in context:
+            if action == 'end' and elem.tag in ['{http://www.aixm.aero/schema/5.1.1/message}hasMember', '{http://www.aixm.aero/schema/5.1/message}hasMember']:
+                feature = aixm_types.parse_feature(elem[0])  # first child of 'hasMember' is aixm feature
+                l.append(feature)
 
     aixm_types.XLink.resolve()
 
